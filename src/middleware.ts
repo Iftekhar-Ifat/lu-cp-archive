9dc54ed6a322bd165413fb97ccd4fa8f0c5fbb5e
Iftekhar-Ifat <iftekharifat007@gmail.com>
2025-02-12 20:12:19 +0600

    update public route matcher to include API clerk endpoint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const publicRoute = createRouteMatcher(["/"]);
+const publicRoute = createRouteMatcher(["/", "/api/clerk"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!publicRoute(request)) {
