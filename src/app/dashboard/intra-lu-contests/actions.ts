a7e3e72696160d197ff4c109cdbd3b829d7e8049
Iftekhar-Ifat <iftekharifat007@gmail.com>
2025-03-07 17:06:44 +0600

    added intra-lu-contests page

---
+++ src/app/dashboard/intra-lu-contests/actions.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { type ContestDifficultyEnum } from "@/utils/types";
+import { type Tag } from "emblor";
+
+const createContestAction = async (data: {
+  name: string;
+  description: string;
+  link: string;
+  tags: Tag[];
+  difficulty: ContestDifficultyEnum;
+}) => {
+  console.log(data);
+  // Simulate network delay
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  // Mock success response
+  return { success: true, message: "Contest created successfully" };
+};
+
+const updateContestAction = async (data: {
+  id: string;
+  name: string;
+  description: string;
+  link: string;
+  tags: Tag[];
+  difficulty: ContestDifficultyEnum;
+}) => {
+  console.log(data);
+  // Simulate network delay
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  // Mock success response
+  return { success: true, message: "Contest created successfully" };
+};
+
+export { createContestAction, updateContestAction };
