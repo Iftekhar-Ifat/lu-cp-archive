f64cad9095c87926a438e387fc779feeccb264f0
Iftekhar-Ifat <iftekharifat007@gmail.com>
2025-05-20 01:21:03 +0600

    fixed topic wise problem error

--- src/app/dashboard/topic-wise/[topic]/_components/topic-progress-section.tsx
+++ src/app/dashboard/topic-wise/[topic]/_components/topic-progress-section.tsx
@@ -7,7 +7,7 @@ import Loading from "../loading";
 import Error from "@/components/shared/error";
 import { getProblemProgressStats } from "../problem-actions";
 import { unwrapActionResult } from "@/utils/error-helper";
-// import { type ProblemDifficulty } from "@/types/types";
+import { type ProblemDifficulty } from "@/types/types";
 
 export default function TopicProgressSection({
   topicSlug,
@@ -23,16 +23,15 @@ export default function TopicProgressSection({
     queryKey: [topicSlug, "progress"],
     queryFn: async () => {
       const result = await getProblemProgressStats(topicSlug);
-      return unwrapActionResult(result);
       // When working with postgres
-      /* const rawProgressData = unwrapActionResult(result);
+      const rawProgressData = unwrapActionResult(result);
       return rawProgressData.map((rawData) => ({
         difficulty: rawData.difficulty as ProblemDifficulty,
         skipped: Number(rawData.skipped),
         inProgress: Number(rawData.inProgress),
         done: Number(rawData.done),
         total: Number(rawData.total),
-      })); */
+      }));
     },
     staleTime: Infinity,
   });
